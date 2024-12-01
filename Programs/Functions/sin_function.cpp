7eccd06179ee2559e7ffcb6456c4e367085fdf10
Alireza Bartari <88055990+AlirezaBartari@users.noreply.github.com>
2024-12-01 23:19:29 +0330

    Update sin_function.cpp

--- Programs/Functions/sin_function.cpp
+++ Programs/Functions/sin_function.cpp
@@ -13,33 +13,36 @@ int main(){
     cin >> degree ;
     
     float sin = sin_calc( degree ) ;
-    cout << "sin " << degree << " = " << sin << endl ;
+    cout << "sin (" << degree << ") = " << sin << endl ;
     
     return 0 ;
 }
 
 float sin_calc( float degree ){
 
-    int flag = 1 ;
-    while(degree >= 180)
-    {
-        degree -= 180 ;
-        flag *= -1 ;
+    // کاهش زاویه به محدوده [0, 360]
+    while (degree >= 360) {
+        degree -= 360;
     }
-    
-    float rad = degree * 0.01745329 ; // or *3.14/180 
-    int sign = 1 ;
+    while (degree < 0) {
+        degree += 360;
+    }
+
+    // تبدیل درجه به رادیان
+    float rad = degree * ( M_PI / 180 ) ;
+    int sign = 1 ; // برای تعیین علامت جملات
     float calc = rad ;
-    
+
+    // محاسبه سری تیلور
     for(float i=3 ; i<=11 ; i+=2 )
     {   
         sign *= -1 ;
         calc += (float)(sign*( power(rad , i) / fact( i ) )) ;
     }
-    calc *= flag ;
+    
     return calc ;
 }
-
+// محاسبه فاکتوریل
 float fact( int n ){
 
     int f = 1 ;
@@ -48,7 +51,7 @@ float fact( int n ){
     
     return f ;
 }
-
+// محاسبه توان
 long double power( float x , float n ){
 
     long double pow = 1  ;
