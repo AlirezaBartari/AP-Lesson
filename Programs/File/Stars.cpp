33014f0c4f6b1dca6e248fa482dd2dc0bba22c8e
Alireza Bartari <88055990+AlirezaBartari@users.noreply.github.com>
2024-12-25 01:07:31 +0330

    Create Stars.cpp

---
+++ Programs/File/Stars.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{   
+    
+    ofstream mf;    // ofstream تعریف یک شی از کلاس  
+    int stars = 1, n = 8;
+    
+    // ایجاد یک فایل جدید در صورت وجود نداشتن فایل با همین نام
+    // و اضافه کردن محتویات جدید به فایل
+    mf.open("folder\\Example.txt");
+
+    //چک کردن اینکه فایل باز شده یا خیر
+    if (!mf.is_open())
+    {
+        cout << "Error opening file";
+        return 1;
+    }
+    
+    for(int i=0 ; i<n ; i++ )
+    {   
+        for(int j=0 ; j< n-i-1 ; j++) 
+            mf << "   ";
+        
+        for(int j=0 ; j<stars ; j++)
+            mf << " * ";
+        
+        mf << endl;    
+        stars += 2; // برای افزایش تعداد ستاره ها در هر خط
+    }
+}   
