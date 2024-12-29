6249a8333a6c2b7b31ceb47c20309f4104f485ab
Alireza Bartari <88055990+AlirezaBartari@users.noreply.github.com>
2024-12-29 14:29:33 +0330

    Create Search_float.cpp

---
+++ Programs/Array/Search_float.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    float arr[10], num;
+
+    cout << "Enter 10 numbers: ";
+    // حلقه اول برای گرفتن ده عدد صحیح
+    for(int i=0 ; i<10 ; i++)
+        cin >> arr[i];
+
+    cout << "Enter number for searching: "; 
+    cin >> num;     // دریافت یک عدد برای جستجو در آرایه
+
+    for(int i=0 ; i<10 ; i++)
+        if(arr[i] == num)
+        {
+            cout << "found" << endl;
+            return 0;   // اگر عدد پیدا شود برنامه خاتمه می یابد
+        }    
+    // .بعد از پیمایش تمام عناصر آرایه اگر عدد پیدا نشد
+    // پیام زیر نمایش داده می شود
+    cout << "not found" << endl; 
+}
